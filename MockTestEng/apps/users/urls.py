12da920567ae09a41471531a8aaaa00276c26fa9
Pritam <pritamkumar747@gmail.com>
2025-07-03 18:47:21 +0530

    Update urls.py

--- MockTestEng/apps/users/urls.py
+++ MockTestEng/apps/users/urls.py
@@ -3,8 +3,10 @@
 from django.urls import path
 from .views import RegisterView, LoginView
 from .views import UserProfileView
-
+from django.http import JsonResponse
 urlpatterns = [
+    
+    path('', lambda request: JsonResponse({'message': 'Users API Root'})),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('me/', UserProfileView.as_view(), name='user-profile'),
