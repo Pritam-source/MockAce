510199db9960b54ff7dbff1180a69d4850c53c0e
Pritam <pritamkumar747@gmail.com>
2025-07-05 22:39:00 +0530

    Update settings.py

--- MockTestEng/MockTestEng/settings.py
+++ MockTestEng/MockTestEng/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
    'apps.users',
@@ -59,7 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = "MockTestEng.urls"
