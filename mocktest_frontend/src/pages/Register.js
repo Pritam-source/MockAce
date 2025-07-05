08f8d019b06b90c0000ace00b1bc7501bdc7b590
Pritam <pritamkumar747@gmail.com>
2025-07-05 22:11:49 +0530

    Update Register.js

--- mocktest_frontend/src/pages/Register.js
+++ mocktest_frontend/src/pages/Register.js
@@ -30,7 +30,7 @@ function Register({ onShowLogin }) {
     setSuccess('');
     setError('');
     try {
-      await axios.post('http://127.0.0.1:8000/api/users/register/', form);
+      await axios.post(`${process.env.REACT_APP_API_URL}/api/users/register/`, form);;
       setSuccess('Registration successful! You can now login.');
       setForm({ username: '', email: '', password: '' });
     } catch (err) {
